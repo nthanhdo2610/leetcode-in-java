730533bf8570dfdce7cb2f456ab87d0272130505
nthanhdo2610 <nthanhdo11110030@gmail.com>
2024-01-21 13:34:11 -0800

    practicing

---
+++ src/main/java/RunLengthDecoding.java
@@ -0,0 +1,25 @@
+public class RunLengthDecoding {
+
+    public static String runLengthDecode(String encoded) {
+
+        StringBuilder sb = new StringBuilder();
+        int crr = 0;
+        for (int i = 0; i < encoded.length(); i++) {
+            if (Character.isLetter(encoded.charAt(i))) {
+                int l = Integer.parseInt(encoded.substring(crr, i));
+                for (int j = 0; j < l; j++) {
+                    sb.append(encoded.charAt(i));
+                }
+                crr = i + 1;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String encodedString = "3A2B10C";
+        String decodedString = runLengthDecode(encodedString);
+        System.out.println(decodedString);
+    }
+}
