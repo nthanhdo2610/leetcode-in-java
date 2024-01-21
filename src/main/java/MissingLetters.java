6ce56c28b8f57159078ffa86836f3df39117e31d
nthanhdo2610 <nthanhdo11110030@gmail.com>
2024-01-21 12:13:40 -0800

    practicing

---
+++ src/main/java/MissingLetters.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MissingLetters {
+
+    public static void findMissingLetters(String sentence) {
+
+        char[] ch = sentence.toLowerCase().toCharArray();
+
+//        Map<Character, Integer> letters = new HashMap<>();
+        Set<Character> set = new HashSet<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+//            letters.put(c, 0);
+            set.add(c);
+        }
+
+        for (char c : ch) {
+            set.remove(c);
+//            letters.remove(c);
+        }
+
+//        letters.keySet().forEach(System.out::print);
+        set.forEach(System.out::print);
+
+    }
+
+    public static void main(String[] args) {
+        String inputSentence = "The quick brown fox jumps over the lazy dog";
+
+        // Call the method to find and print missing letters
+        findMissingLetters("yxz");
+    }
+}
