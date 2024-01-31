65cf13d1a5728749f05e674baa267d20efb2179b
nthanhdo2610 <nthanhdo11110030@gmail.com>
2024-01-31 10:49:32 -0800

    practicing

--- src/main/java/LeakMemoryProblem.java
+++ src/main/java/LeakMemoryProblem.java
@@ -8,14 +8,41 @@ public class LeakMemoryProblem {
 
     static void leakMemory() throws InterruptedException {
 
-        GarbageCollection collection1 = new GarbageCollection(List.of("collection1"));
-        GarbageCollection collection2 = new GarbageCollection(List.of("collection2"));
-        GarbageCollection collection3 = new GarbageCollection(List.of("collection3"));
+        MyCollection collection1 = new MyCollection(List.of("collection1"));
+        MyCollection collection2 = new MyCollection(List.of("collection2"));
+
+        MyCollection collection3 = collection1;
 
         collection1 = null;
         collection2 = null;
 
         System.gc();
         Thread.sleep(3000);
+
+        collection3 = null;
+        System.gc();
+        Thread.sleep(3000);
+    }
+}
+
+class MyCollection {
+
+    private final List<String> collections;
+
+    public MyCollection(List<String> collections) {
+        this.collections = collections;
+        System.out.println("Object created: " + this);
+    }
+
+    @Override
+    protected void finalize() {
+        System.out.println("Object garbage collected: " + this);
+    }
+
+    @Override
+    public String toString() {
+        return "Garbage{" +
+                "collections=" + collections +
+                '}';
     }
 }
